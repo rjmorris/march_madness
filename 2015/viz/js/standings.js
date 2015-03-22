27769bee20b426b96f65116948ba63954a57479b
Joey Morris <rjmorris@rti.org>
2015-03-22 10:44:38 -0400

    Assigned colors, variables, and labels in the standings dynamically based on the current round.

--- 2015/viz/js/standings.js
+++ 2015/viz/js/standings.js
@@ -7,6 +7,76 @@ var margin = {
 var width = 950 - margin.left - margin.right;
 var height = 850 - margin.top - margin.bottom;
 
+var currentRound = 2;
+
+var roundLabelsPlayed = [
+    'Round of 32',
+    'Sweet 16',
+    'Elite 8',
+    'Final 4',
+    'Final',
+    'Champion'
+];
+
+var roundLabelsUnplayed = [
+    'Round of 32 (best case)',
+    'Sweet 16 (best case)',
+    'Elite 8 (best case)',
+    'Final 4 (best case)',
+    'Final (best case)',
+    'Champion (best case)'
+];
+
+var roundLabels = roundLabelsPlayed.slice(0, currentRound).concat(roundLabelsUnplayed.slice(currentRound));
+
+var sorter = {
+    'Round of 32 (best case)': 'sort_cumul_best32',
+    'Sweet 16 (best case)': 'sort_cumul_best16',
+    'Elite 8 (best case)': 'sort_cumul_best8',
+    'Final 4 (best case)': 'sort_cumul_best4',
+    'Final (best case)': 'sort_cumul_best2',
+    'Champion (best case)': 'sort_cumul_best1',
+    'Round of 32': 'sort_cumul32',
+    'Sweet 16': 'sort_cumul16',
+    'Elite 8': 'sort_cumul8',
+    'Final 4': 'sort_cumul4',
+    'Final': 'sort_cumul2',
+    'Champion': 'sort_cumul1'
+};
+
+var cumul_scorer = {
+    'Round of 32 (best case)': 'cumul_best_score32',
+    'Sweet 16 (best case)': 'cumul_best_score16',
+    'Elite 8 (best case)': 'cumul_best_score8',
+    'Final 4 (best case)': 'cumul_best_score4',
+    'Final (best case)': 'cumul_best_score2',
+    'Champion (best case)': 'cumul_best_score1',
+    'Round of 32': 'cumul_score32',
+    'Sweet 16': 'cumul_score16',
+    'Elite 8': 'cumul_score8',
+    'Final 4': 'cumul_score4',
+    'Final': 'cumul_score2',
+    'Champion': 'cumul_score1'
+};
+
+var colorsPlayed = [
+    "#d1d8ff",
+    "#a7b0e8",
+    "#7d88d2",
+    "#5360bb",
+    "#2938a5",
+    "#00108f"
+];
+
+var colorsUnplayed = [
+    "#949494",
+    "#a2a2a2",
+    "#b0b0b0",
+    "#bebebe",
+    "#cccccc",
+    "#dbdbdb"
+];
+
 var svg = d3.select("#standings")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -24,7 +94,7 @@ var y = d3.scale.ordinal()
 ;
 
 var color = d3.scale.ordinal()
-    .range(["#00108f", "#949494", "#a2a2a2", "#b0b0b0", "#bebebe", "#cccccc", "#dbdbdb"])
+    .range(colorsPlayed.slice(-currentRound).concat(colorsUnplayed.slice(0, -currentRound)))
 ;
 
 var xAxisTop = d3.svg.axis()
@@ -41,34 +111,20 @@ var yAxis = d3.svg.axis()
     .orient("left")
 ;
 
-var sorter = {
-    'Round of 32': 'sort_cumul32',
-    'Sweet 16 (best case)': 'sort_cumul_best16',
-    'Elite 8 (best case)': 'sort_cumul_best8',
-    'Final 4 (best case)': 'sort_cumul_best4',
-    'Final (best case)': 'sort_cumul_best2',
-    'Champion (best case)': 'sort_cumul_best1'
-};
-
-
 var data;
 
 d3.csv("data/standings.csv", function(error, inputData) {
     data = inputData;
 
-    color.domain(d3.keys(sorter));
+    color.domain(roundLabels);
 
     data.forEach(function(d) {
         d3.values(sorter).forEach(function(v) {
             d[v] = +d[v];
         });
-
-        d['Round of 32'] = +d['cumul_score32'];
-        d['Sweet 16 (best case)'] = +d['cumul_best_score16'];
-        d['Elite 8 (best case)'] = +d['cumul_best_score8'];
-        d['Final 4 (best case)'] = +d['cumul_best_score4'];
-        d['Final (best case)'] = +d['cumul_best_score2'];
-        d['Champion (best case)'] = +d['cumul_best_score1'];
+        d3.keys(cumul_scorer).forEach(function(v) {
+            d[v] = +d[cumul_scorer[v]];
+        });
 
         d.flag_unofficial = d.flag_unofficial == "1";
 
@@ -83,7 +139,7 @@ d3.csv("data/standings.csv", function(error, inputData) {
     });
 
     data.sort(function(a, b) {
-        return d3.descending(a[sorter['Round of 32']], b[sorter['Round of 32']]);
+        return d3.descending(a[sorter[roundLabels[currentRound-1]]], b[sorter[roundLabels[currentRound-1]]]);
     });
 
     x.domain([0, 219]).nice();
